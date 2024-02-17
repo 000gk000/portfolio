d2cfdd342cab503cbe80a04d0a6513d2f8bd05b1
portfoliO <sivagk314@gmail.com>
2024-02-17 10:43:04 +0530

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import HomePage from "./components/HomePage"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+   
+    <HomePage/>
+    </>
+       
+    
+  )
+}
+
+export default App
---
+++ portfolio/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+          <>
+           
+           hello
+          
+          </>
+    )
+}
+
+export default HomePage;
